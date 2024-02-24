fcb7ec11c27a9e846d7b8cbe3281cb1dee106bf5
martin-creator <martinlubowa@outlook.com>
2024-02-24 20:56:12 +0300

    Created movie model

--- moviemate/movielist_app/models.py
+++ moviemate/movielist_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    genre = models.CharField(max_length=100)
+    year = models.IntegerField()
+    director = models.CharField(max_length=100)
+    plot = models.TextField()
+    poster = models.URLField(max_length=300)
+    trailer = models.URLField(max_length=300)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
