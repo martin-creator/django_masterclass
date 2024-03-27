6480b08e5f777cad67c57d3e631bd67e6a56b780
martin-creator <martinlubowa@outlook.com>
2024-03-27 17:30:50 +0300

    Updated models character limit

--- moviemate/movielist_app/models.py
+++ moviemate/movielist_app/models.py
@@ -3,13 +3,13 @@ from django.db import models
 # Create your models here.
 
 class Movie(models.Model):
-    title = models.CharField(max_length=100)
-    genre = models.CharField(max_length=100)
+    title = models.CharField(max_length=300)
+    genre = models.CharField(max_length=300)
     year = models.IntegerField()
-    director = models.CharField(max_length=100)
+    director = models.CharField(max_length=300)
     plot = models.TextField()
-    poster = models.URLField(max_length=300)
-    trailer = models.URLField(max_length=300)
+    poster = models.URLField(max_length=3000)
+    trailer = models.URLField(max_length=3000)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
