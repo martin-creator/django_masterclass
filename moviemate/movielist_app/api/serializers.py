c6e132271c31f868788e343feac2ab7830efe489
martin-creator <martinlubowa@outlook.com>
2024-03-27 17:31:09 +0300

    Added create on serializer

--- moviemate/movielist_app/api/serializers.py
+++ moviemate/movielist_app/api/serializers.py
@@ -17,6 +17,31 @@ class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = ['id', 'title', 'genre', 'year', 'director', 'plot', 'poster', 'trailer', 'created_at', 'updated_at']
+        # id = serializers.IntegerField(read_only=True)
+        # title = serializers.CharField(max_length=100)
+        # genre = serializers.CharField(max_length=100)
+        # year = serializers.IntegerField()
+        # director = serializers.CharField(max_length=100)
+        # plot = serializers.CharField()
+        # poster = serializers.URLField(max_length=300)
+        # trailer = serializers.URLField(max_length=300)
+        # created_at = serializers.DateTimeField()
+        # updated_at = serializers.DateTimeField()
+
+    
+    def create(self, validated_data):
+        return Movie.objects.create(**validated_data)
+    
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get('title', instance.title) # get the value of title from validated_data and if it is not present, then get the value of title from instance
+        instance.genre = validated_data.get('genre', instance.genre)
+        instance.year = validated_data.get('year', instance.year)
+        instance.director = validated_data.get('director', instance.director)
+        instance.plot = validated_data.get('plot', instance.plot)
+        instance.poster = validated_data.get('poster', instance.poster)
+        instance.trailer = validated_data.get('trailer', instance.trailer)
+        instance.save()
+        return instance
 
 # class MovieSerializer(serializers.ModelSerializer):
 # class meta is a class within a class 
