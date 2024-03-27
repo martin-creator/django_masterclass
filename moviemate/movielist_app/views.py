94c6c9e933ccb2a6ae68e0f0b1fa40ad4c943f65
martin-creator <martinlubowa@outlook.com>
2024-03-27 15:44:07 +0300

    Added movie detail function

--- moviemate/movielist_app/views.py
+++ moviemate/movielist_app/views.py
@@ -7,3 +7,16 @@ def movie_list(request):
     movies = Movie.objects.all() # get all movies from database in QuerySet. A queryset is a list of objects of a given model
     data = {"movies": list(movies.values())} # convert QuerySet to list of dictionaries
     return JsonResponse(data)
+
+def movie_detail(request, pk):
+    movie = Movie.objects.get(pk=pk) # get movie from database
+    data = {"movie": {
+        "title": movie.title,
+        "genre": movie.genre,
+        "year": movie.year,
+        "director": movie.director,
+        "plot": movie.plot,
+        "poster": movie.poster,
+        "trailer": movie.trailer
+    }}
+    return JsonResponse(data)
