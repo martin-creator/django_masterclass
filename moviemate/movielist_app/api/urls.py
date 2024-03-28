659e795e9ce4176b4ab6f42e9585448636286ec6
martin-creator <martinlubowa@outlook.com>
2024-03-28 21:18:10 +0300

    Updated urls

--- moviemate/movielist_app/api/urls.py
+++ moviemate/movielist_app/api/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path, include
-from movielist_app.api.views import movie_list, movie_detail
+# from movielist_app.api.views import movie_list, movie_detail
+from movielist_app.api.views import MovieListCreateAPIView, MovieDetailAPIView
+
+# urlpatterns = [
+#     path('list/', movie_list, name='movie_list'),
+#     path('<int:pk>/', movie_detail, name='movie_detail'),
+# ]
 
 urlpatterns = [
-    path('list/', movie_list, name='movie_list'),
-    path('<int:pk>/', movie_detail, name='movie_detail'),
+    path('list/', MovieListCreateAPIView.as_view(), name='movie_list'),
+    path('<int:pk>/', MovieDetailAPIView.as_view(), name='movie_detail'),
 ]
+
+# we use as_view() method to convert our class-based views into function-based views.
