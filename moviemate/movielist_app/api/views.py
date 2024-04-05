9bb5a5a4513990d870f15fd7b79a6e929718d503
martin-creator <martinlubowa@outlook.com>
2024-04-05 23:05:33 +0300

    Updated watchlist and streamplatform views

--- moviemate/movielist_app/api/views.py
+++ moviemate/movielist_app/api/views.py
@@ -2,17 +2,17 @@ from rest_framework import status
 # from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from movielist_app.models import Movie
-from movielist_app.api.serializers import MovieSerializer
+from movielist_app.models import WatchList, StreamPlatform
+from movielist_app.api.serializers import WatchListSerializer, StreamPlatformSerializer
 
-class MovieListCreateAPIView(APIView):
+class WatchListCreateAPIView(APIView):
     def get(self, request):
-        movies = Movie.objects.all()
-        serializer = MovieSerializer(movies, many=True)
+        movies = WatchList.objects.all()
+        serializer = WatchListSerializer(movies, many=True)
         return Response(serializer.data)
     
     def post(self, request):
-        serializer = MovieSerializer(data=request.data)
+        serializer = WatchListSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
@@ -20,24 +20,60 @@ class MovieListCreateAPIView(APIView):
     
 
 
-class MovieDetailAPIView(APIView):
+class WatchDetailAPIView(APIView):
     def get(self, request, pk):
-        movie = Movie.objects.get(pk=pk)
-        serializer = MovieSerializer(movie)
+        movie = WatchList.objects.get(pk=pk)
+        serializer = WatchListSerializer(movie)
         return Response(serializer.data)
     
     def put(self, request, pk):
-        movie = Movie.objects.get(pk=pk)
-        serializer = MovieSerializer(movie, data=request.data)
+        movie = WatchList.objects.get(pk=pk)
+        serializer = WatchListSerializer(movie, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
     def delete(self, request, pk):
-        movie = Movie.objects.get(pk=pk)
+        movie = WatchList.objects.get(pk=pk)
         movie.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+    
+
+class StreamPlatformListAPIView(APIView):
+    def get(self, request):
+        platforms = StreamPlatform.objects.all()
+        serializer = StreamPlatformSerializer(platforms, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = StreamPlatformSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=400)
+    
+
+class StreamPlatformDetailAPIView(APIView):
+    def get(self, request, pk):
+        platform = StreamPlatform.objects.get(pk=pk)
+        serializer = StreamPlatformSerializer(platform)
+        return Response(serializer.data)
+    
+    def put(self, request, pk):
+        platform = StreamPlatform.objects.get(pk=pk)
+        serializer = StreamPlatformSerializer(platform, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk):
+        platform = StreamPlatform.objects.get(pk=pk)
+        platform.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
 
 # @api_view(['GET', 'POST'])
 # def movie_list(request):
--- moviemate/movielist_app/views.py
+++ moviemate/movielist_app/views.py
@@ -1,22 +1,22 @@
-from django.shortcuts import render
-from movielist_app.models import Movie
-from django.http import JsonResponse
+# from django.shortcuts import render
+# from movielist_app.models import WatchList, StreamPlatform
+# from django.http import JsonResponse
 
 
-def movie_list(request):
-    movies = Movie.objects.all() # get all movies from database in QuerySet. A queryset is a list of objects of a given model
-    data = {"movies": list(movies.values())} # convert QuerySet to list of dictionaries
-    return JsonResponse(data)
+# def watch_list(request):
+#     movies = Movie.objects.all() # get all movies from database in QuerySet. A queryset is a list of objects of a given model
+#     data = {"movies": list(movies.values())} # convert QuerySet to list of dictionaries
+#     return JsonResponse(data)
 
-def movie_detail(request, pk):
-    movie = Movie.objects.get(pk=pk) # get movie from database
-    data = {"movie": {
-        "title": movie.title,
-        "genre": movie.genre,
-        "year": movie.year,
-        "director": movie.director,
-        "plot": movie.plot,
-        "poster": movie.poster,
-        "trailer": movie.trailer
-    }}
-    return JsonResponse(data)
+# def watch_detail(request, pk):
+#     movie = Movie.objects.get(pk=pk) # get movie from database
+#     data = {"movie": {
+#         "title": movie.title,
+#         "genre": movie.genre,
+#         "year": movie.year,
+#         "director": movie.director,
+#         "plot": movie.plot,
+#         "poster": movie.poster,
+#         "trailer": movie.trailer
+#     }}
+#     return JsonResponse(data)
