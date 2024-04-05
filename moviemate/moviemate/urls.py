909bf039e217a7cb1f42f2eb879ffa522c7994d7
martin-creator <martinlubowa@outlook.com>
2024-04-06 00:26:54 +0300

    Changed name from movie to watch

--- moviemate/moviemate/urls.py
+++ moviemate/moviemate/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('movie/', include('movielist_app.api.urls')),
+    path('watch/', include('movielist_app.api.urls')),
 ]
