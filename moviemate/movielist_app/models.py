b0c9702d4955381b9a17996e66b605164dbd7046
martin-creator <martinlubowa@outlook.com>
2024-04-05 23:05:09 +0300

    Created streamplatform model

--- moviemate/movielist_app/models.py
+++ moviemate/movielist_app/models.py
@@ -2,16 +2,35 @@ from django.db import models
 
 # Create your models here.
 
-class Movie(models.Model):
-    title = models.CharField(max_length=300)
-    genre = models.CharField(max_length=300)
-    year = models.IntegerField()
-    director = models.CharField(max_length=300)
-    plot = models.TextField()
-    poster = models.URLField(max_length=3000)
-    trailer = models.URLField(max_length=3000)
+class StreamPlatform(models.Model):
+    name = models.CharField(max_length=300)
+    about = models.TextField()
+    website = models.URLField(max_length=300)
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class WatchList(models.Model):
+    title = models.CharField(max_length=300)
+    storyline = models.TextField()
+    active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
+
+# class Movie(models.Model):
+#     title = models.CharField(max_length=300)
+#     genre = models.CharField(max_length=300)
+#     year = models.IntegerField()
+#     director = models.CharField(max_length=300)
+#     plot = models.TextField()
+#     poster = models.URLField(max_length=3000)
+#     trailer = models.URLField(max_length=3000)
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     updated_at = models.DateTimeField(auto_now=True)
+
+#     def __str__(self):
+#         return self.title
