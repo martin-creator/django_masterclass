b44528d09da49db3953c02604506143efb9fb07d
martin-creator <martinlubowa@outlook.com>
2024-04-06 10:50:06 +0300

    Registered reviews

--- moviemate/movielist_app/admin.py
+++ moviemate/movielist_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from movielist_app.models import WatchList, StreamPlatform
+from movielist_app.models import WatchList, StreamPlatform, Review
 
 # Register your models here.
 
 admin.site.register(WatchList)
 admin.site.register(StreamPlatform)
+admin.site.register(Review)
