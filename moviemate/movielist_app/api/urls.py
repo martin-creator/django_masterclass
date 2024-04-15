8ceaee4a54b498beecb8299f35afeb2ba59aa9e6
martin-creator <martinlubowa@outlook.com>
2024-04-15 19:38:59 +0300

    Regestired mixins url

--- moviemate/movielist_app/api/urls.py
+++ moviemate/movielist_app/api/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path, include
 # from movielist_app.api.views import movie_list, movie_detail
-from movielist_app.api.views import WatchListCreateAPIView, WatchDetailAPIView, StreamPlatformListAPIView, StreamPlatformDetailAPIView
-
+from movielist_app.api.views import WatchListCreateAPIView, WatchDetailAPIView, StreamPlatformListAPIView, StreamPlatformDetailAPIView, ReviewListCreateAPIView
 # urlpatterns = [
 #     path('list/', movie_list, name='movie_list'),
 #     path('<int:pk>/', movie_detail, name='movie_detail'),
@@ -13,6 +12,8 @@ urlpatterns = [
 
     path('stream/', StreamPlatformListAPIView.as_view(), name='stream_list'),
     path('stream/<int:pk>/', StreamPlatformDetailAPIView.as_view(), name='streamplatform-detail'),
+
+    path('review/,', ReviewListCreateAPIView.as_view(), name='review-list')
 ]
 
 # we use as_view() method to convert our class-based views into function-based views.
