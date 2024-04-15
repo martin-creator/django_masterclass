d9a41402962de5c389e482a9a7a22a5729b18049
martin-creator <martinlubowa@outlook.com>
2024-04-15 20:29:22 +0300

    Used routers

--- moviemate/movielist_app/api/urls.py
+++ moviemate/movielist_app/api/urls.py
@@ -1,17 +1,25 @@
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
 # from movielist_app.api.views import movie_list, movie_detail
-from movielist_app.api.views import WatchListCreateAPIView, WatchDetailAPIView, StreamPlatformListAPIView, StreamPlatformDetailAPIView, ReviewListCreateAPIView, ReviewDetailAPIView, ReviewCreateAPIView
+from movielist_app.api.views import (WatchListCreateAPIView, WatchDetailAPIView, StreamPlatformListAPIView, StreamPlatformDetailAPIView, 
+                                     ReviewListCreateAPIView, ReviewDetailAPIView, ReviewCreateAPIView
+                                     , StreamPlatformVS)
 # urlpatterns = [
 #     path('list/', movie_list, name='movie_list'),
 #     path('<int:pk>/', movie_detail, name='movie_detail'),
 # ]
 
+router = DefaultRouter()
+router.register('stream', StreamPlatformVS, basename='streamplatform')
+
 urlpatterns = [
     path('list/', WatchListCreateAPIView.as_view(), name='movie_list'),
     path('<int:pk>/', WatchDetailAPIView.as_view(), name='movie_detail'),
 
-    path('stream/', StreamPlatformListAPIView.as_view(), name='stream_list'),
-    path('stream/<int:pk>/', StreamPlatformDetailAPIView.as_view(), name='streamplatform-detail'),
+    path('', include(router.urls)),
+    # path('stream/', StreamPlatformListAPIView.as_view(), name='stream_list'),
+    # path('stream/<int:pk>/', StreamPlatformDetailAPIView.as_view(), name='streamplatform-detail'),
 
     # path('review/,', ReviewListCreateAPIView.as_view(), name='review-list'),
     path('stream/<int:pk>/review/', ReviewCreateAPIView.as_view(), name='review-create'),
