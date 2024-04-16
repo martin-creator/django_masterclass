8eb9254649b46e98b8fd57c459ec599dfd74460c
martin-creator <martinlubowa@outlook.com>
2024-04-16 22:03:51 +0300

    Added avg ratings to views

--- moviemate/movielist_app/api/views.py
+++ moviemate/movielist_app/api/views.py
@@ -30,6 +30,14 @@ class ReviewCreateAPIView(generics.CreateAPIView):
         if review_queryset.exists():
             raise serializers.ValidationError('You have already reviewed this movie')
         
+        if watchlist.number_rating == 0:
+            watchlist.avg_rating = serializer.validated_data['rating']
+        else:
+            watchlist.avg_rating = (watchlist.avg_rating + serializer.validated_data['rating']) / 2
+
+        watchlist.number_rating = watchlist.number_rating + 1
+        watchlist.save()
+        
         # Set the watchlist object to the review object
         serializer.save(watchlist=watchlist, review_user=review_user)
 
