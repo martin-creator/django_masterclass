93bdf17ab128bcf615126f04fc0cd5a403815406
martin-creator <martinlubowa@outlook.com>
2024-04-16 15:54:12 +0300

    Added user ti review view

--- moviemate/movielist_app/api/views.py
+++ moviemate/movielist_app/api/views.py
@@ -1,4 +1,4 @@
-from rest_framework import status, generics, mixins, viewsets
+from rest_framework import status, generics, mixins, viewsets, serializers
 # from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -8,16 +8,28 @@ from django.shortcuts import get_object_or_404
 
 
 
+
 class ReviewCreateAPIView(generics.CreateAPIView):
     serializer_class = ReviewSerializer
+
+    def get_queryset(self):
+        return Review.objects.all()
 
     def perform_create(self, serializer):
         # Get the watchlist id from the URL
         watchlist_pk = self.kwargs['pk']
+
         # Get the watchlist object
         watchlist = WatchList.objects.get(pk=watchlist_pk)
+
+        review_user = self.request.user
+        review_queryset = Review.objects.filter(watchlist=watchlist, review_user=review_user)
+
+        if review_queryset.exists():
+            raise serializers.ValidationError('You have already reviewed this movie')
+        
         # Set the watchlist object to the review object
-        serializer.save(watchlist=watchlist)
+        serializer.save(watchlist=watchlist, review_user=review_user)
 
 
 class ReviewListCreateAPIView(generics.ListAPIView):
