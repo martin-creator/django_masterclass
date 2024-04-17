796f35ba0fde4a0bd037d0b1bd392393b9482c66
martin-creator <martinlubowa@outlook.com>
2024-04-17 22:28:49 +0300

    update serializers.py with save method

--- moviemate/user_app/api/serializers.py
+++ moviemate/user_app/api/serializers.py
@@ -9,9 +9,18 @@ class RegisterSerializer(serializers.ModelSerializer):
         fields = ['username', 'email' 'password', 'password2']
         extra_kwargs = {'password': {'write_only': True}}
 
-    # def create(self, validated_data):
-    #     user = User.objects.create_user(
-    #         validated_data['username'],
-    #         password=validated_data['password']
-    #     )
-    #     return user
+    def save(self):
+        password = self.validated_data['password']
+        password2 = self.validated_data['password2']
+
+        if password != password2:
+            raise serializers.ValidationError({'password': 'Passwords must match.'})
+        
+        if User.objects.filter(email=self.validated_data['email']).exists():
+            raise serializers.ValidationError({'email': 'Email already exists.'})
+        
+        account = User(email=self.validated_data['email'], username=self.validated_data['username'])
+        account.set_password(password)
+        account.save()
+
+        return account
