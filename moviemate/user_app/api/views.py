edda8caef8695d53aabc03aca0ffb12c10973eb5
martin-creator <martinlubowa@outlook.com>
2024-04-17 22:46:34 +0300

    Added generation logic to view

--- moviemate/user_app/api/views.py
+++ moviemate/user_app/api/views.py
@@ -2,14 +2,26 @@ from rest_framework.decorators import api_view
 from user_app.api.serializers import RegisterSerializer
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.authtoken.models import Token
 
 
 @api_view(['POST'])
 def registration_view(request):
 
     if request.method == 'POST':
+
         serializer = RegisterSerializer(data=request.data)
+        
         data = {}
+
         if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            account = serializer.save()
+            data['response'] = 'Successfully registered a new user.'
+            data['username'] = account.username
+            data['email'] = account.email
+
+            token = Token.objects.get(user=account).key
+        else:
+            data = serializer.errors
+
+        return Response(data)
