70ed231a785ba39e8f69b3881dd6a3227f7c775b
martin-creator <martinlubowa@outlook.com>
2024-04-17 22:20:32 +0300

    update urls.py with register route

--- moviemate/user_app/api/urls.py
+++ moviemate/user_app/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
+from user_app.api.views import registration_view
 
 urlpatterns = [
     path('login/', obtain_auth_token, name='login'),
+    path('register/', registration_view, name='register'),
 ]
