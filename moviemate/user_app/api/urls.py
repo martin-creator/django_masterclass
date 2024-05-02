abf79b9efe775325cc56ed582b5a98a7abf7e187
martin-creator <martinlubowa@outlook.com>
2024-05-02 21:43:21 +0300

    update urls.py

--- moviemate/user_app/api/urls.py
+++ moviemate/user_app/api/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
-from user_app.api.views import registration_view
+from user_app.api.views import registration_view, logout_view
+from rest_framework_simplejwt.views import (TokenObtainPairView,TokenRefreshView,)
+
 
 urlpatterns = [
     path('login/', obtain_auth_token, name='login'),
     path('register/', registration_view, name='register'),
+    path('logout/', logout_view, name='logout'),
+
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
