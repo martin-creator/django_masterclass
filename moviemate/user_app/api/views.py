927079e9f0b8929e878612fdb6a23862d2ca36ff
martin-creator <martinlubowa@outlook.com>
2024-05-02 23:28:37 +0300

    update views.py

--- moviemate/user_app/api/views.py
+++ moviemate/user_app/api/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authtoken.models import Token
 from rest_framework_simplejwt.tokens import RefreshToken
-from user_app import models
+# from user_app import models
 
 
 @api_view(['POST'])
