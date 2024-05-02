3701d21bdd0afbe51e55b0fb00445320c4d9fda4
martin-creator <martinlubowa@outlook.com>
2024-05-03 00:54:30 +0300

    update settings.py

--- moviemate/moviemate/settings.py
+++ moviemate/moviemate/settings.py
@@ -144,7 +144,9 @@ REST_FRAMEWORK = {
     # ],
     'DEFAULT_THROTTLE_RATES': {
         'anon': '1/day',
-        'user': '10/day'
+        'user': '10/day',
+        'review-create': '1/day',
+        'review-list-create': '10/day',
     }
 }
 
