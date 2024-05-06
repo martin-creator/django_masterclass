906dafe3c3ed7079232889f8bf9fa03703dec39a
martin-creator <martinlubowa@outlook.com>
2024-05-06 10:18:46 +0300

    update urls.py

--- taskmanager/todolist_app/urls.py
+++ taskmanager/todolist_app/urls.py
@@ -1,7 +1,5 @@
-from django.contrib import admin
-from django.urls import path, include
+from todolist_app import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('task/', include('todolist_app.urls')),
+    path('/', views.todolist, name='todolist'),
 ]
