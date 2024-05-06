1683635793a8354c309d5c41babb893210574fd4
martin-creator <martinlubowa@outlook.com>
2024-05-06 11:49:15 +0300

    update urls.py

--- taskmanager/todolist_app/urls.py
+++ taskmanager/todolist_app/urls.py
@@ -1,5 +1,10 @@
 from todolist_app import views
+from django.urls import path
 
 urlpatterns = [
-    path('/', views.todolist, name='todolist'),
+    path('', views.todolist, name='todolist'),
+    path('delete/<task_id>', views.delete_task, name='delete_task'),
+    path('edit/<task_id>', views.edit_task, name='edit_task'),
+    path('complete/<task_id>', views.complete_task, name='complete_task'),
+    path('pending/<task_id>', views.pending_task, name='pending_task'),
 ]
